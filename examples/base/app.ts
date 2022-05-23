a674ff54d48a08beb64f9c72c3b16112488e0a5d
keshanwen <3640@winnermedical.com>
2022-05-23 09:21:08 +0800

    feat：处理请求body的数据

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -65,3 +65,21 @@ axios({
   method: 'get',
   url: '/base/get#hash',
 })
+
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const arr = new Int32Array([21, 31])
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
